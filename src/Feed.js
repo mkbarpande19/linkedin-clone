c3f81fcd12d52b3cf8c41d39ce317764c4e0a00d
Mayur Barpande <mayurbarpande9019@gmail.com>
2021-07-10 20:17:37 +0530

    Added Posts Dynamically to the firebase database

---
+++ src/Feed.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import CreateIcon from "@material-ui/icons/Create";
+import "./Feed.css";
+import InputOptions from "./InputOptions";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import Posts from "./Posts";
+import { db } from "./firebase";
+import firebase from "firebase";
+
+
+const Feed = () => {
+  
+  const [input, setInput] = useState(null);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, []);
+
+  const sendPosts = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      name: "Mayur Barpande",
+      description: "this is test",
+      message: input,
+      photoUrl: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    });
+  };
+  return (
+    <div className="feed">
+      <div className="feed_inputContainer">
+        <div className="feed_input">
+          <CreateIcon />
+          <form>
+            <input value={input} onChange={(e)=>setInput(e.target.value)} type="text" />
+            <button onClick={sendPosts} type="submit">
+              Send
+            </button>
+          </form>
+        </div>
+
+        <div className="feed_inputOption">
+          <InputOptions Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOptions
+            Icon={SubscriptionsIcon}
+            title="Video"
+            color="#E7A33E"
+          />
+          <InputOptions Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOptions
+            Icon={CalendarViewDayIcon}
+            title="Write Article"
+            color="#7FC15E"
+          />
+        </div>
+      </div>
+      {posts.map(({id, data : {name, description, message, photoUrl}}) => (
+        <Posts 
+            key={id}
+            name={name}
+            description={description}
+            message={message}
+            photourl={photoUrl}
+        />
+      ))}
+      
+    </div>
+  );
+};
+
+export default Feed;
---
+++ src/Posts.js
@@ -0,0 +1,34 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import InputOptions from './InputOptions';
+import "./Posts.css";
+import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
+
+const Posts = ({name, description, message, photourl}) => {
+    return (
+        <div className="post">
+            <div className="post_header">
+                <Avatar/>
+                <div className="post_info">
+                    <h2>{name}</h2>
+                    <p>{description}</p>
+                </div>
+            </div>
+            <div className="post_body">
+                <p>{message}</p>
+            </div>
+
+            <div className="post_buttons">
+                <InputOptions Icon={ThumbUpOutlinedIcon} title="Like" color="gray" />
+                <InputOptions Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+                <InputOptions Icon={ShareOutlinedIcon} title="Share" color="gray" />
+                <InputOptions Icon={SendOutlinedIcon} title="Send" color="gray" />
+            </div>
+        </div>
+    )
+}
+
+export default Posts
