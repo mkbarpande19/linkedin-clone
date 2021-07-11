178917f6e900c64dc040141133ed11ad5c395456
Mayur Barpande <mayurbarpande9019@gmail.com>
2021-07-11 19:17:58 +0530

    LinkedIn clone successfully completed

--- src/Posts.js
+++ src/Posts.js
@@ -1,34 +1,34 @@
-import { Avatar } from '@material-ui/core';
-import React from 'react';
-import InputOptions from './InputOptions';
+import { Avatar } from "@material-ui/core";
+import React, { forwardRef } from "react";
+import InputOptions from "./InputOptions";
 import "./Posts.css";
-import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
-import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
-import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
-import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
+import ThumbUpOutlinedIcon from "@material-ui/icons/ThumbUpOutlined";
+import ChatOutlinedIcon from "@material-ui/icons/ChatOutlined";
+import ShareOutlinedIcon from "@material-ui/icons/ShareOutlined";
+import SendOutlinedIcon from "@material-ui/icons/SendOutlined";
 
-const Posts = ({name, description, message, photourl}) => {
-    return (
-        <div className="post">
-            <div className="post_header">
-                <Avatar src={photourl}>{name[0]}</Avatar>
-                <div className="post_info">
-                    <h2>{name}</h2>
-                    <p>{description}</p>
-                </div>
-            </div>
-            <div className="post_body">
-                <p>{message}</p>
-            </div>
-
-            <div className="post_buttons">
-                <InputOptions Icon={ThumbUpOutlinedIcon} title="Like" color="gray" />
-                <InputOptions Icon={ChatOutlinedIcon} title="Comment" color="gray" />
-                <InputOptions Icon={ShareOutlinedIcon} title="Share" color="gray" />
-                <InputOptions Icon={SendOutlinedIcon} title="Send" color="gray" />
-            </div>
+const Posts = forwardRef(({ name, description, message, photourl }, ref) => {
+  return (
+    <div ref={ref} className="post">
+      <div className="post_header">
+        <Avatar src={photourl}>{name[0]}</Avatar>
+        <div className="post_info">
+          <h2>{name}</h2>
+          <p>{description}</p>
         </div>
-    )
-}
+      </div>
+      <div className="post_body">
+        <p>{message}</p>
+      </div>
 
-export default Posts
+      <div className="post_buttons">
+        <InputOptions Icon={ThumbUpOutlinedIcon} title="Like" color="gray" />
+        <InputOptions Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+        <InputOptions Icon={ShareOutlinedIcon} title="Share" color="gray" />
+        <InputOptions Icon={SendOutlinedIcon} title="Send" color="gray" />
+      </div>
+    </div>
+  );
+});
+
+export default Posts;
